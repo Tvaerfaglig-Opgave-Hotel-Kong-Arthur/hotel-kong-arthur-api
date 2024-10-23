56a69e60924617359bb00c43efa1cdbd7a073c73
DetGrey <anenovlar@gmail.com>
2024-10-23 08:50:19 +0200

    port added to all services and gateways

--- admin_gateway/app.py
+++ admin_gateway/app.py
@@ -4,9 +4,23 @@
 '''
 
 from flask import Flask, jsonify, request
+import requests
 
 app = Flask(__name__)
+
+BASE_PCS_URL = f'http://product_catalog_service:5000/products'
+BASE_CS_URL = f'http://cart_service:5001/cart'
 
 @app.route('/', methods=['GET'])
 def all():
     return
+
+# ------------------------------------------------------ CART SERVICE
+# Get cart items
+@app.route('/cart', methods=['GET'])
+def get_cart_items():
+    url = f'{BASE_CS_URL}'
+    req = requests.get(url)
+    return jsonify(req.json()), req.status_code
+
+app.run(debug=True, host='0.0.0.0', port=5000)
